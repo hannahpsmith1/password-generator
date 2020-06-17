e4242b463e7a2abd0fbe6d446cf6db6f13d8ffc8
Hannah Smith <hannahpsmith1@gmail.com>
2020-06-17 16:45:47 -0700

    removed unneccessary code that was commented out

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-var promptLength = prompt ("Password Length between 8-128 characters");
-var confirmUpper = confirm ("Do you want CAPITALS included?");
-var confirmLower = confirm ("Do you want lowercase letters?");
-var confirmNum  = confirm ("Do you want to use numbers 0-9?");
-var confirmSpecial = confirm ("Do you want to use special characters");
-
-// console.log(promptLength);
-// console.log(confirmUpper);
-// console.log(confirmUpper);
-// console.log(confirmNum);
-// console.log(confirmSpecial);
 
 var stringUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var stringLower = "abcdefghijklmnopqrstuvwxyz"
 var stringNum ="0123456789"
 var stringSpecial = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-var length = Number(promptLength)
 
 function getLower (){
   var randomIndex = Math.floor(Math.random() * stringLower.length)
   console.log(stringLower [randomIndex])
   return stringLower[randomIndex]
 }
-getLower();
+// console.log(getLower) to test
 
 function getUpper (){
   var randomIndex = Math.floor(Math.random() * stringUpper.length)
@@ -33,7 +20,7 @@ function getUpper (){
   return stringUpper[randomIndex]
 
 }
-getUpper();
+// console.log(getUpper) to test
 
 function getNum (){
   var randomIndex = Math.floor(Math.random() * stringNum.length)
@@ -41,7 +28,7 @@ function getNum (){
   return stringNum[randomIndex]
 
 }
-getNum();
+// console.log(getNum) to test
  
 function getSpecial (){
   var randomIndex = Math.floor(Math.random() * stringSpecial.length)
@@ -49,21 +36,21 @@ function getSpecial (){
   return stringSpecial[randomIndex]
 
 }
-// if (confirmUpper === true) {
-//   options=options.concat(stringUpper)
-// }
-// if (confirmLower === true){
-//   options=options.concat(stringLower)
-// }
-// console.log(options)
-// for(i=0; i<length; i++){
-//   var randomIndex = Math.floor(Math.random() * options.length)
-//   result=result.concat(options [randomIndex])
-// }
+// console.log(getSpecial) to test
+
+
 
 function generatePassword (){
+  var promptLength = prompt ("Password Length between 8-128 characters");
+  var confirmUpper = confirm ("Do you want CAPITALS included?");
+  var confirmLower = confirm ("Do you want lowercase letters?");
+  var confirmNum  = confirm ("Do you want to use numbers 0-9?");
+  var confirmSpecial = confirm ("Do you want to use special characters");
   var result = ""
   var options = ""
+  var length = Number(promptLength)
+
+
   if (confirmUpper === true) {
     options=options.concat(stringUpper)
   }
