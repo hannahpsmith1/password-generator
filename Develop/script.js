e5d6e91605998c73bc2dd77feee82159b03df5c3
Hannah Smith <hannahpsmith1@gmail.com>
2020-06-17 16:17:14 -0700

    filling in empty fuctions from tutoring session

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,8 @@ var generateBtn = document.querySelector("#generate");
 var promptLength = prompt ("Password Length between 8-128 characters");
 var confirmUpper = confirm ("Do you want CAPITALS included?");
 var confirmLower = confirm ("Do you want lowercase letters?");
-// var confirmNum  = confirm ("Do you want to use numbers 0-9?");
-// var confirmSpecial = confirm ("Do you want to use special characters");
+var confirmNum  = confirm ("Do you want to use numbers 0-9?");
+var confirmSpecial = confirm ("Do you want to use special characters");
 
 // console.log(promptLength);
 // console.log(confirmUpper);
@@ -18,10 +18,7 @@ var stringUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var stringLower = "abcdefghijklmnopqrstuvwxyz"
 var stringNum ="0123456789"
 var stringSpecial = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-// var options = ""
 var length = Number(promptLength)
-
-// var result = ""
 
 function getLower (){
   var randomIndex = Math.floor(Math.random() * stringLower.length)
@@ -31,13 +28,25 @@ function getLower (){
 getLower();
 
 function getUpper (){
+  var randomIndex = Math.floor(Math.random() * stringUpper.length)
+  console.log(stringUpper [randomIndex])
+  return stringUpper[randomIndex]
 
 }
+getUpper();
 
 function getNum (){
+  var randomIndex = Math.floor(Math.random() * stringNum.length)
+  console.log(stringNum [randomIndex])
+  return stringNum[randomIndex]
+
 }
+getNum();
  
 function getSpecial (){
+  var randomIndex = Math.floor(Math.random() * stringSpecial.length)
+  console.log(stringSpecial [randomIndex])
+  return stringSpecial[randomIndex]
 
 }
 // if (confirmUpper === true) {
@@ -61,7 +70,12 @@ function generatePassword (){
   if (confirmLower === true){
     options=options.concat(stringLower)
   }
-  console.log(options)
+  if (confirmNum === true){
+    options=options.concat(stringNum)
+  }
+  if (confirmSpecial === true){
+    options=options.concat(stringSpecial)
+  }
   for(i=0; i<length; i++){
     var randomIndex = Math.floor(Math.random() * options.length)
     result=result.concat(options [randomIndex])
