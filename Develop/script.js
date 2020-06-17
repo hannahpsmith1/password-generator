8d1ccf445bd67ee2e53fa336a57897481fed6cbf
Hannah Smith <hannahpsmith1@gmail.com>
2020-06-17 14:52:26 -0700

    added empty functions

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+var promptLength = prompt ("Password Length between 8-128 characters");
+var confirmUpper = confirm ("Do you want CAPITALS included?");
+var confirmLower = confirm ("Do you want lowercase letters?");
+var confirmNum  = confirm ("Do you want to use numbers 0-9?");
+var confirmSpecial = confirm ("Do you want to use special characters");
+
+function getLower (){
+
+}
+
+function getUpper (){
+
+}
+
+function getNum (){
+
+}
+ 
+function getSpecial (){
+  
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
