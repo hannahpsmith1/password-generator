cbc1f2c2af83b277c1011eaba4a296b8bbcdd927
Hannah Smith <hannahpsmith1@gmail.com>
2020-06-18 08:39:22 -0700

    added error validation for number of characters in password

--- Develop/script.js
+++ Develop/script.js
@@ -41,14 +41,26 @@ function getSpecial (){
 
 
 function generatePassword (){
-  var promptLength = prompt ("Password Length between 8-128 characters");
+  var promptLength = Number (prompt ("Password Length between 8-128 characters"));
+  while (isNaN(promptLength) || promptLength < 8 || promptLength > 128) promptLength = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+  // this adds a condition that password length must be between numbers 8 and 128.  The isNaN() function determines whether a value is an illegal number
+
   var confirmUpper = confirm ("Do you want CAPITALS included?");
   var confirmLower = confirm ("Do you want lowercase letters?");
   var confirmNum  = confirm ("Do you want to use numbers 0-9?");
   var confirmSpecial = confirm ("Do you want to use special characters");
+
+  // while (!confirmUpper && !confirmLower && !confirmNum && !confirmSpecial) {
+  //   alert("You must select at least one character type!");
+  //   confirmUpper = confirm("Would you like to use uppercase letters?");
+  //   confirmLower = confirm("Would you like to use lowercase letters?");
+  //   confirmNum = confirm("Would you like to use numbers?");
+  //   confirmSpecial = confirm("Would you like to use special characters?");
+
+
+  
   var result = ""
   var options = ""
-  var length = Number(promptLength)
 
 
   if (confirmUpper === true) {
@@ -63,13 +75,20 @@ function generatePassword (){
   if (confirmSpecial === true){
     options=options.concat(stringSpecial)
   }
-  for(i=0; i<length; i++){
+  for(i=0; i<promptLength; i++){
     var randomIndex = Math.floor(Math.random() * options.length)
     result=result.concat(options [randomIndex])
   }
 
 return result
 }
+
+// if (promptLength < 8) {
+//   alert ("password must be at least 8 characters long.")
+//   else {
+//     writePassword()
+//   }
+// }
 
 
 function writePassword() {
@@ -82,3 +101,39 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+// clipboard.addEventListener('click', () => {
+// 	const textarea = document.createElement('textarea');
+// 	const password = resultEl.innerText;
+	
+// 	if(!password) { return; }
+	
+// 	textarea.value = password;
+// 	document.body.appendChild(textarea);
+// 	textarea.select();
+// 	document.execCommand('copy');
+// 	textarea.remove();
+// 	alert('Password copied to clipboard');
+// });
+
+// function copyPassword(){
+
+//   document.getElementById("display").select();
+
+//   document.execCommand("Copy");
+
+//   alert("Password copied to clipboard!");
+
+// }$('#form-password').on('change', function(){
+//   pass = $('#form-password').val();
+//   if(pass.length < 6){
+//       $('#passwordmsg').html('Password must be at least 6 characters');
+//   }else {
+//       $('#passwordmsg').html("");
+  
+//   });
